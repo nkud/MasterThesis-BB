6d674c9c7a43d67859eb4e5c6759562bab05ae08
Naoki Ueda <su104003@gmail.com>
2015-03-16 19:05:23 +0900

    create master.py

---
+++ CancerImmunoediting/master/master.py
@@ -0,0 +1,16 @@
+import os
+
+ls = os.listdir('.')
+
+out = open('cancercell-size-master.txt', 'w')
+for l in ls:
+    n = 0
+    if not 'result' in l: continue
+    cancerfilepass = '%s/bin/cancercell-size.txt' % l
+    file = open(cancerfilepass, 'r')
+    for line in file:
+        n += 1
+        line = line.split()
+        out.write('%s,' % line[1])
+        #if n > 300: break
+    out.write('\n')
