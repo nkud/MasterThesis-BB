2fb2d4fa33b82f5469c49748f1ffdcba90f97d61
Naoki Ueda <su104003@gmail.com>
2015-06-02 16:15:07 +0900

    T細胞をマップ出力

--- CancerImmunoediting/script/stat.py
+++ CancerImmunoediting/script/stat.py
@@ -34,6 +34,7 @@ for line in source_file:
         print paramline
         config_line.append(paramline)
 
+###############################################################################
 #
 # 通常のグラフスクリプトを生成する。
 #
@@ -73,11 +74,12 @@ auto_plot_line += 'replot;set output;'
 for line in auto_plot_line:
     auto_plot_file.write(line)
 
+###############################################################################
 #
 # アニメーション用のプロットスクリプトを生成する。
 #
 
-# 酸素マップアニメーション
+# 酸素マップアニメーション ################################################################
 # animation.plt
 animation_plot_file = open('oxygen-animation.plt', 'w')
 
@@ -105,6 +107,7 @@ frame_plot_line += 'file(n)=sprintf("../bin/%d-oxygen.txt", n);'
 frame_plot_line += 'set title title(n);'
 frame_plot_line += 'set view map;'
 frame_plot_line += 'set cbrange[0:10];'
+frame_plot_line += 'set xlabel "OXYGEN MAP";'
 frame_plot_line += 'splot file(n) w pm3d;'
 frame_plot_line += 'if(n<%d) n=n+1; reread;' % MAX_STEP
 # -----------------------------------------------
@@ -112,7 +115,7 @@ frame_plot_line += 'if(n<%d) n=n+1; reread;' % MAX_STEP
 for line in frame_plot_line:
     frame_plot_file.write(line)
 
-# グルコースマップアニメーション
+# グルコースマップアニメーション #############################################################
 # animation.plt
 animation_plot_file = open('glucose-animation.plt', 'w')
 
@@ -140,6 +143,7 @@ frame_plot_line += 'file(n)=sprintf("../bin/%d-glucose.txt", n);'
 frame_plot_line += 'set title title(n);'
 frame_plot_line += 'set view map;'
 frame_plot_line += 'set cbrange[0:10];'
+frame_plot_line += 'set xlabel "GLUCOSE MAP";'
 frame_plot_line += 'splot file(n) w pm3d;'
 frame_plot_line += 'if(n<%d) n=n+1; reread;' % MAX_STEP
 # -----------------------------------------------
@@ -147,7 +151,7 @@ frame_plot_line += 'if(n<%d) n=n+1; reread;' % MAX_STEP
 for line in frame_plot_line:
     frame_plot_file.write(line)
 
-# 細胞マップアニメーション
+# 細胞マップアニメーション ################################################################
 # animation.plt
 animation_plot_file = open('animation.plt', 'w')
 
@@ -175,6 +179,7 @@ frame_plot_line += 'file(n)=sprintf("../bin/%d-cell.txt", n);'
 frame_plot_line += 'set title title(n);'
 frame_plot_line += 'set view map;'
 frame_plot_line += 'set cbrange[0:1];'
+frame_plot_line += 'set xlabel "NORMAL & CANCER MAP";'
 frame_plot_line += 'splot file(n) w pm3d;'
 frame_plot_line += 'if(n<%d) n=n+1; reread;' % MAX_STEP
 # -----------------------------------------------
@@ -182,6 +187,43 @@ frame_plot_line += 'if(n<%d) n=n+1; reread;' % MAX_STEP
 for line in frame_plot_line:
     frame_plot_file.write(line)
 
+
+# T細胞マップアニメーション ###############################################################
+animation_plot_file = open('tcell-animation.plt', 'w')
+
+animation_plot_line = []
+# -----------------------------------------------
+animation_plot_line += 'set terminal gif animate optimize size 200,200 delay 5;'
+animation_plot_line += 'set output "tcell-animation.gif";'
+animation_plot_line += 'set style line 1 lw 2;'
+animation_plot_line += 'set key below right;'
+animation_plot_line += 'set key textcolor lt 0;'
+animation_plot_line += 'n=1;'
+animation_plot_line += 'load "tcell-frame.plt";'
+# -----------------------------------------------
+
+for line in animation_plot_line:
+    animation_plot_file.write(line)
+
+# frame.plt
+frame_plot_file = open('tcell-frame.plt', 'w')
+
+frame_plot_line = []
+# -----------------------------------------------
+frame_plot_line += 'title(n)=sprintf("t = %d", n);'
+frame_plot_line += 'file(n)=sprintf("../bin/%d-tcell.txt", n);'
+frame_plot_line += 'set title title(n);'
+frame_plot_line += 'set view map;'
+frame_plot_line += 'set cbrange[0:1];'
+frame_plot_line += 'set xlabel "TCELL MAP";'
+frame_plot_line += 'splot file(n) w pm3d;'
+frame_plot_line += 'if(n<%d) n=n+1; reread;' % MAX_STEP
+# -----------------------------------------------
+
+for line in frame_plot_line:
+    frame_plot_file.write(line)
+
+###############################################################################
 #
 # index.html
 #
@@ -229,7 +271,7 @@ for line in config_line:
 html_line += '</table>'
 html_line += '<hr />'
 
-html_line += image_set_line('animation.gif', 'glucose-animation.gif', 'oxygen-animation.gif')
+html_line += image_set_line('animation.gif', 'tcell-animation.gif', 'glucose-animation.gif', 'oxygen-animation.gif')
 html_line += '<hr />'
 html_line += '<h2>%s</h2>\n' % '平均細胞エネルギー'
 html_line += image_set_line('cell-energy-average.png')
